8aff8226282ed127bb8c11ff4f0381312ea0f325
losolio <losvik+github@gmail.com>
2020-06-01 00:00:51 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Heading from './Heading';
-import OpenBadge from './OpenBadge';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Heading from "./Heading";
+import OpenBadge from "./OpenBadge";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React',
+      name: "React",
       openStatus: false
+    };
+
+    const openingTimes = {
+      monday: [
+        { opens: "08:00", closes: "11:00" },
+        { opens: "13:00", closes: "15:00" }
+      ],
+      tuesday: [{ opens: "09:00", closes: "17:30" }],
+      wednesday: [{ opens: "09:00", closes: "03:00" }],
+      thursday: [{ opens: "09:00", closes: "16:00" }],
+      friday: [{ opens: "09:00", closes: "16:30" }],
+      saturday: [{ opens: "09:00", closes: "16:30" }],
+      sunday: [{ opens: "09:00", closes: "16:00" }]
     };
   }
 
@@ -23,4 +36,4 @@ class App extends Component {
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
